a99c44fdd598d6384a57d84d446ade9dfae019e8
Petra <petra.skokic@gmail.com>
2023-11-10 14:10:22 +0100

    Finalizing connection between interfaces
    - Finalized connection between Interface_main and Interface_add_plants.
    - Moved PyFloraPot class into a separate script so it can be imported into all interfaces.
    - Added basic functionalities into the Interface_add_plants.

--- Interface_add_pots.py
+++ Interface_add_pots.py
@@ -3,6 +3,9 @@ import tkinter as tk
 import sqlite3
 from tkinter import messagebox
 
+from PyFlora_class import PyFloraPot
+# from Interface_main import PyFloraPot
+
 
 os.chdir('PyFlora Pots')
 
@@ -13,7 +16,96 @@ class InterfaceAddPots:
         self.toplevel_add_pots = tk.Toplevel(root)
         self.toplevel_add_pots.title("PyFlora Pots - Let's plant!")
         self.toplevel_add_pots.geometry('1200x800')
+        self.interface_elements()
+
+    def get_all_known_plant_names(self):
+        """Returns the list of all plant names available in the lexicon."""
+
+        DB_NAME = 'Database_plants_lexicon.db'
+        QUERY_GET_ALL_KNOWN_PLANT_NAMES = 'SELECT plant_name FROM Database_plants_lexicon'
+
+        ALL_KNOWN_PLANT_NAMES = []
+
+        try:
+            with sqlite3.connect(DB_NAME) as sql_connection:
+                cursor = sql_connection.cursor()
+
+                cursor.execute(QUERY_GET_ALL_KNOWN_PLANT_NAMES)
+                data = cursor.fetchall()
+                for plant_name in data:
+                    ALL_KNOWN_PLANT_NAMES.append(plant_name[0])
+
+                print("All plant names retrived.")
+                return ALL_KNOWN_PLANT_NAMES
+
+        except sqlite3.Error as e:
+            print('Data retrieving unsucessful. Error: ', e)
+            messagebox.showerror(title='Error in retrieving data!',
+                                 message='Data retrieving unsucessful. Error: ' + str(e))
+
+    def validate_choices(self):
+        """Validates choices when creating a new PyFlora pot."""
+        # validate the chosen pot_name is available
+        for i in range(0, PyFloraPot.count_pots):
+            if self.new_pot.get() == PyFloraPot.list_pots[i].pot_name:
+                validation = 0
+                messagebox.showwarning(title='PyFlora pot already in use!',
+                                       message='Please choose another name for the PyFlora pot.')
+        # check a plant is chosen from the menu
+        if self.chosen_plant.get() == self.DEFAULT_PLANT:
+            validation = 0
+            messagebox.showwarning(title='No plant chosen!',
+                                   message='Please choose a plant from the plant menu.\n\nNote: If the plant you have in mind is not on the list, add it to the plant lexicon.')
+        # return validation result
+        if validation == 0:
+            return False
+        else:
+            return True
+
+    def add_new_pot(self):
+        """Button fuction - adds a new PyFlora pot to the database."""
+
+        if not self.validate_choices():
+            print("Not validated")
+        else:
+            print("Validated")
+
+        # Create text variables for PyFlora pot specifics
+
+    def interface_elements(self):
+
+        # Define interface variables
+
+        self.new_pot = tk.StringVar()
+
+        self.chosen_plant = tk.StringVar()
+        self.DEFAULT_PLANT = "Choose a plant from the list:"
+        self.chosen_plant.set(self.DEFAULT_PLANT)
+
+        all_known_plant_names = self.get_all_known_plant_names()
+
+        # Define interface elements
+
+        pot_name_label = tk.Label(
+            self.toplevel_add_pots, text="Give this pot a name:")
+        pot_name_label.grid(row=1, column=1)
+
+        entry_pot_name = tk.Entry(
+            self.toplevel_add_pots, textvariable=self.new_pot)
+        entry_pot_name.grid(row=1, column=2)
+
+        plant_label = tk.Label(self.toplevel_add_pots,
+                               text="Choose a plant to plant:")
+        plant_label.grid(row=2, column=1)
+
+        plant_menu = tk.OptionMenu(
+            self.toplevel_add_pots, self.chosen_plant, *all_known_plant_names)
+        plant_menu.grid(row=2, column=2)
+
+        add_pot_button = tk.Button(
+            self.toplevel_add_pots, text="Let's plant!!", command=self.add_new_pot)
+        add_pot_button.grid(row=3, columnspan=3)
 
         exit_button = tk.Button(self.toplevel_add_pots, text="Quit",
                                 command=self.toplevel_add_pots.destroy)
-        exit_button.pack()
+        exit_button.grid(row=1000, columnspan=3)
--- Interface_main.py
+++ Interface_main.py
@@ -3,41 +3,45 @@ import tkinter as tk
 import sqlite3
 from tkinter import messagebox
 
-from Interface_add_pots import *
+from PyFlora_class import PyFloraPot
+from Interface_add_pots import InterfaceAddPots
 
+# os.chdir('PyFlora Pots')
 
+"""
 class PyFloraPot:
 
-    PyFloraPots_count = 0
+    count_pots = 0
 
     def __init__(self, pot_name, plant_name):
         self.pot_name = pot_name
         self.plant_name = plant_name
-        PyFloraPot.PyFloraPots_count += 1
+        PyFloraPot.count_pots += 1
 
+    def update_pot_list(self):
+        DB_NAME = 'Database_PyFlora_Pots.db'
+        QUERY_GET_ALL_POTS = 'SELECT PyFlora_pot_name, plant_name FROM Database_PyFlora_Pots'
 
-def update_pot_list():
-    DB_NAME = 'Database_PyFlora_Pots.db'
-    QUERY_GET_ALL_POTS = 'SELECT PyFlora_pot_name, plant_name FROM Database_PyFlora_Pots'
+        list_pots = []
 
-    PyFloraPot_list = []
+        try:
+            with sqlite3.connect(DB_NAME) as sql_connection:
+                cursor = sql_connection.cursor()
 
-    try:
-        with sqlite3.connect(DB_NAME) as sql_connection:
-            cursor = sql_connection.cursor()
+                cursor.execute(QUERY_GET_ALL_POTS)
+                data = cursor.fetchall()
+                for pot in data:
+                    pot_class = PyFloraPot(pot[0], pot[1])
+                    list_pots.append(pot_class)
 
-            cursor.execute(QUERY_GET_ALL_POTS)
-            data = cursor.fetchall()
-            for pot in data:
-                pot_class = PyFloraPot(pot[0], pot[1])
-                PyFloraPot_list.append(pot_class)
+                print("All PyFlora Pot names retrived.")
+                return list_pots
 
-            print("All PyFlora Pot names retrived.")
-            return PyFloraPot_list
+        except sqlite3.Error as e:
+            messagebox.showerror(title='Error in retrieving data',
+                                 message='Data retrieving unsucessful. Error: ' + str(e))
 
-    except sqlite3.Error as e:
-        messagebox.showerror(title='Error in retrieving data',
-                             message='Data retrieving unsucessful. Error: ' + e)
+"""
 
 
 class InterfaceMain:
@@ -46,9 +50,9 @@ class InterfaceMain:
         self.root.title("PyFlora Pots")
         self.root.geometry('1200x800')
 
-        PyFloraPot_list = update_pot_list()
+        PyFloraPot_list = PyFloraPot.update_pot_list(self)
 
-        for i in range(0, PyFloraPot.PyFloraPots_count):
+        for i in range(0, PyFloraPot.count_pots):
             button = tk.Button(self.root, text=PyFloraPot_list[i].pot_name)
             button.pack()
 
