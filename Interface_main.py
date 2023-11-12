c67c417d3955bde4f5d84cc230cc6038d9d75645
Petra <petra.skokic@gmail.com>
2023-11-12 18:36:17 +0100

    InterfaceMain - Button order
    Added if/else to calculate the postition of each button on the grid

--- Interface_main.py
+++ Interface_main.py
@@ -22,20 +22,25 @@ class InterfaceMain:
 
         PyFloraPot_list = PyFloraPot.update_pot_list(self)
 
-        print("PyFlora count: ", PyFloraPot.count_pots)
-        print("PyFlora list in main: ", PyFloraPot_list)
-
         for i in range(0, PyFloraPot.count_pots):
+
+            if i % 2 == 0:
+                button_row = i
+                button_column = 1
+            else: 
+                button_row = i - 1
+                button_column = 2
+
             button = tk.Button(self.root, text=PyFloraPot_list[i].pot_name)
-            button.pack()
+            button.grid(row=button_row, column=button_column)
 
         add_button = tk.Button(self.root, text="Add new pot",
                             command=self.launch_InterfaceAddPots)
-        add_button.pack()
+        add_button.grid(row=button_row + 1, column=1, columnspan=2)
 
         exit_button = tk.Button(self.root, text="Quit",
                                 command=self.root.destroy)
-        exit_button.pack()
+        exit_button.grid(row=button_row + 2, column=1, columnspan=2)
 
 
 
